008cbd1bba0e596e99ef8f04d1b54ef78d21c611
Colin Schatz <cschatz@mills.edu>
2021-10-07 10:45:10 -0700

    Update

--- src/Day12.java
+++ src/Day12.java
@@ -1,3 +1,15 @@
 public class Day12 {
 	
+	public static void main(String[] args) {
+		Stack<String> pancakes = new LinkedStack<String>();	
+		pancakes.push("blueberry");
+		pancakes.push("strawberry");
+		pancakes.push("chocolate chip");
+		
+		while (!pancakes.isEmpty()) {
+			String p = pancakes.pop();
+			System.out.println(p);
+		}
+	}
+
 }
