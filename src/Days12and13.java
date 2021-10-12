6646d401e34e941786c0c93477c3984e2e42cd77
Colin Schatz <cschatz@mills.edu>
2021-10-12 10:11:59 -0700

    Update

--- src/Days12and13.java
+++ src/Days12and13.java
@@ -1,16 +1,36 @@
+import java.util.Scanner;
+
 public class Days12and13
 {
+	public static boolean checkParens(String text) {
+		
+	}
 	
 	public static void main(String[] args) {
-		Stack<String> pancakes = new LinkedStack<String>();	
-		pancakes.push("blueberry");
-		pancakes.push("strawberry");
-		pancakes.push("chocolate chip");
 		
-		while (!pancakes.isEmpty()) {
-			String p = pancakes.pop();
-			System.out.println(p);
+		Scanner scanner = new Scanner(System.in);
+		
+		while (true) {
+			System.out.println("Type an expression (blank to stop): ");
+			String expression = scanner.nextLine();
+			if (expression.equals(""))
+				break;
+			if (checkParens(expression))
+				System.out.println("Syntax ok.");
+			else
+				System.out.println("Syntax NOT ok!");
 		}
+		
+		
+//		Stack<String> pancakes = new ArrayStack<String>();	
+//		pancakes.push("blueberry");
+//		pancakes.push("strawberry");
+//		pancakes.push("chocolate chip");
+//		
+//		while (!pancakes.isEmpty()) {
+//			String p = pancakes.pop();
+//			System.out.println(p);
+//		}
 	}
 
 }
